c17d1bef520a754e5e857367a3e3f32e8bea0394
Johnny346 <johnnymul346@gmail.com>
2019-08-08 12:50:55 +0100

    add express

--- clientApp/CoreUI-AngularJS/AngularJS_Starter_GULP/src/js/controllers/LoginController.js
+++ clientApp/CoreUI-AngularJS/AngularJS_Starter_GULP/src/js/controllers/LoginController.js
@@ -1,3 +1,7 @@
-angular.module('app').controller('LoginController', ['$scope', function($scope){
-    $scope.message = "welcome to my app";
+angular.module('app').controller('LoginController', ['$scope', 
+    	function($scope){
+            //$scope.message = "welcome to my app";
+            $scope.login = function() {
+                $scope.message = "welcome to my app " + $scope.user.email;
+            }
 }]);
--- clientApp/CoreUI-AngularJS/AngularJS_Starter_GULP/src/js/controllers/RegisterController.js
+++ clientApp/CoreUI-AngularJS/AngularJS_Starter_GULP/src/js/controllers/RegisterController.js
@@ -1,15 +1,18 @@
-angular.module('app').controller('RegisterController', ['$scope', function($scope){
-    $scope.message = "welcome to my app, here you can register";
-
-    $scope.passwordsMatch = function(pass, repass){
-       if (pass !== repass ){
-           console.log(pass);
-           console.log(repass);
-           document.getElementById("repeatpassword1").style.border = "1px solid red";
-           return true;
-       }else {
-            document.getElementById("repeatpassword1").style.border = "";
-            return false;
-       }
+angular.module('app').controller('RegisterController', ['$scope', 
+    function($scope){
+        
+        $scope.passwordsMatch = function(pass, repass){
+        if (pass !== repass ){
+            console.log(pass);
+            console.log(repass);
+            document.getElementById("repeatpassword1").style.border = "1px solid red";
+            return true;
+        }else {
+                document.getElementById("repeatpassword1").style.border = "";
+                return false;
+        }
+    };
+    $scope.register = function() {
+        $scope.message = "welcome to my app " + $scope.user.email;
     };
 }]);
