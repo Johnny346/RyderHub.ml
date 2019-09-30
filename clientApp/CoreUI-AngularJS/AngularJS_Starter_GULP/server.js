c3a08a1ee4cfe9047ba6ad6d3fb956557326ae9a
Johnny346 <johnnymul346@gmail.com>
2019-09-30 22:53:36 +0100

    fix link to jar file in app.js, change server.js to serve dist folder

--- clientApp/CoreUI-AngularJS/AngularJS_Starter_GULP/server.js
+++ clientApp/CoreUI-AngularJS/AngularJS_Starter_GULP/server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 /**
  * This server should host angular appliation with or without base-url
- * The angular static resources should be under `./public`
+ * The angular static resources should be under `./dist`
  */
 var app = express();
 app.use(function(req, res, next) {
@@ -14,12 +14,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/base-here-if-any', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/base-here-if-any', express.static(path.join(__dirname, 'dist')))
 
 app.get('*', function(req, res, next) {
   //Path to your main file
-  res.status(200).sendFile(path.join(__dirname + '/public/index.html'));
+  res.status(200).sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 
